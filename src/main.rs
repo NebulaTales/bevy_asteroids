133dc92dc2b088410c38390f0571b25a090f58cc
Oragon Efreet <kevin.dorange@protonmail.com>
2021-02-22 15:56:07 +0100

    Rotation and movements basis ok

--- src/main.rs
+++ src/main.rs
@@ -15,36 +15,39 @@ struct Acceleration {
 #[derive(Debug)]
 struct Thrust {
     forward: f32,
-    backward: f32,
     yaw: f32,
 }
 
 impl Default for Thrust {
     fn default() -> Self {
         Thrust {
-            forward: 200.0,
-            backward: 150.0,
-            yaw: 0.03,
+            forward: 500.0,
+            yaw: 0.2,
         }
     }
 }
 
 fn velocity_system(time: Res<Time>, mut query: Query<(&Velocity, Mut<Transform>)>) {
     let delta_time = f32::min(0.2, time.delta_seconds());
+
     for (velocity, mut transform) in query.iter_mut() {
+        transform.rotate(Quat::from_rotation_z(velocity.rotation));
         transform.translation.x += velocity.translation.x * delta_time;
         transform.translation.y += velocity.translation.y * delta_time;
-        //transform.rotation = Quat::from_rotation_z(velocity.rotation);
-        transform.rotate(Quat::from_rotation_z(velocity.rotation));
     }
 }
 
-fn acceleration_system(time: Res<Time>, mut query: Query<(&Acceleration, Mut<Velocity>)>) {
+fn acceleration_system(
+    time: Res<Time>,
+    mut query: Query<(&Acceleration, &Transform, Mut<Velocity>)>,
+) {
     let delta_time = f32::min(0.2, time.delta_seconds());
 
-    for (acceleration, mut velocity) in query.iter_mut() {
+    for (acceleration, transform, mut velocity) in query.iter_mut() {
         velocity.rotation += acceleration.rotation * delta_time;
-        let angle = std::f32::consts::PI / 2.0 + velocity.rotation;
+
+        let rotation = transform.rotation.to_axis_angle();
+        let angle = std::f32::consts::PI / 2.0 + rotation.0.z * rotation.1;
         velocity.translation += Vec2::new(
             angle.cos() * acceleration.forward * delta_time,
             angle.sin() * acceleration.forward * delta_time,
@@ -55,7 +58,6 @@ fn acceleration_system(time: Res<Time>, mut query: Query<(&Acceleration, Mut<Vel
 /// The thrust system adds creates the acceleration
 fn thrust_system(keyboard: Res<Input<KeyCode>>, mut query: Query<(&Thrust, Mut<Acceleration>)>) {
     let forwards = keyboard.pressed(KeyCode::Up);
-    let backwards = keyboard.pressed(KeyCode::Down);
     let left = keyboard.pressed(KeyCode::Left);
     let right = keyboard.pressed(KeyCode::Right);
 
@@ -63,7 +65,6 @@ fn thrust_system(keyboard: Res<Input<KeyCode>>, mut query: Query<(&Thrust, Mut<A
         acceleration.rotation =
             if left { thrust.yaw } else { 0.0 } - if right { thrust.yaw } else { 0.0 };
         acceleration.forward = if forwards { thrust.forward } else { 0.0 }
-            - if backwards { thrust.backward } else { 0.0 };
     }
 }
 
