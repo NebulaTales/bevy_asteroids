6da05efc589a4532fecd13e0cc45790ea6dee0ff
Kevin d'Orange <kevin.dorange@protonmail.com>
2021-04-23 22:03:21 +0200

    Player respawn on death

--- src/player.rs
+++ src/player.rs
@@ -6,7 +6,7 @@ use crate::{
 use bevy::{
     app::{AppBuilder, EventReader, Plugin},
     asset::{AssetServer, Assets, Handle},
-    core::Timer,
+    core::{Time, Timer},
     ecs::{
         entity::Entity,
         query::With,
@@ -20,51 +20,62 @@ use bevy::{
 pub struct Player;
 
 pub fn destroy_on_collision(
-    _: Commands,
+    mut commands: Commands,
     mut events: EventReader<CollisionEvent>,
     q_player: Query<Entity, With<Player>>,
 ) {
     for collision in events.iter() {
-        if let Ok(_) = q_player.get(collision.source) {
-            println!("BOOM!");
+        if let Ok(e) = q_player.get(collision.source) {
+            commands.entity(e).despawn();
+            commands.spawn().insert(SpawnPlayer::default());
         }
     }
 }
 
-pub struct SpawnPlayer;
+pub struct SpawnPlayer(Timer);
+
+impl Default for SpawnPlayer {
+    fn default() -> Self {
+        SpawnPlayer(Timer::from_seconds(3.0, false))
+    }
+}
+
 pub struct PlayerMaterial(Handle<ColorMaterial>);
 
 pub fn spawn_player(
     mut commands: Commands,
+    time: Res<Time>,
     player_material: Res<PlayerMaterial>,
-    q_spawn: Query<Entity, With<SpawnPlayer>>,
+    mut q_spawn: Query<(Entity, &mut SpawnPlayer)>,
 ) {
-    for entity in q_spawn.iter() {
-        commands
-            .entity(entity)
-            .remove::<SpawnPlayer>()
-            .insert_bundle(SpriteBundle {
-                material: player_material.0.clone(),
-                transform: Transform::from_translation(Vec3::new(0.0, -300.0, 0.0)),
-                ..Default::default()
-            })
-            .insert(Velocity::with_translation(0.0, 100.0))
-            .insert(Acceleration::default())
-            .insert(Thrust::default())
-            .insert(Friction(1.0))
-            .insert(DelayedAdd(
-                PlayerControlled,
-                Timer::from_seconds(2.0, false),
-            ))
-            .insert(Collider2D {
-                shape: Shape2D::Circle(32.0),
-                ..Default::default()
-            })
-            .insert(CollisionLayer(PLAYER))
-            .insert(CollisionMask(OBSTACLE))
-            .insert(Wrap::default())
-            .insert(Player)
-            .insert(FireAngleError(0.03));
+    for (entity, mut spawn) in q_spawn.iter_mut() {
+        if spawn.0.tick(time.delta()).just_finished() {
+            commands
+                .entity(entity)
+                .remove::<SpawnPlayer>()
+                .insert_bundle(SpriteBundle {
+                    material: player_material.0.clone(),
+                    transform: Transform::from_translation(Vec3::new(0.0, -300.0, 0.0)),
+                    ..Default::default()
+                })
+                .insert(Velocity::with_translation(0.0, 200.0))
+                .insert(Acceleration::default())
+                .insert(Thrust::default())
+                .insert(Friction(1.0))
+                .insert(DelayedAdd(
+                    PlayerControlled,
+                    Timer::from_seconds(2.0, false),
+                ))
+                .insert(Collider2D {
+                    shape: Shape2D::Circle(32.0),
+                    ..Default::default()
+                })
+                .insert(CollisionLayer(PLAYER))
+                .insert(CollisionMask(OBSTACLE))
+                .insert(Wrap::default())
+                .insert(Player)
+                .insert(FireAngleError(0.03));
+        }
     }
 }
 
@@ -77,7 +88,7 @@ pub fn startup(
 
     let player_material = materials.add(ship_handle.into());
     commands.insert_resource(PlayerMaterial(player_material.clone()));
-    commands.spawn().insert(SpawnPlayer);
+    commands.spawn().insert(SpawnPlayer::default());
 }
 
 pub struct PlayerPlugin;
