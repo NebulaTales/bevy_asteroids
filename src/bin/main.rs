8f604ccec2cc746a9effa0da7c23e6bf27b5ddd7
Kevin d'Orange <kevin.dorange@protonmail.com>
2021-05-03 00:52:59 +0200

    Add entity counter

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,5 +1,22 @@
 use asteroid::AsteroidsGamePlugins;
-use bevy::{app::App, render::color::Color, render::pass::ClearColor, DefaultPlugins};
+use bevy::{
+    app::App,
+    asset::AssetServer,
+    ecs::{
+        query::With,
+        system::{Commands, IntoExclusiveSystem, IntoSystem, Res},
+        world::World,
+    },
+    math::Vec3,
+    render::color::Color,
+    render::pass::ClearColor,
+    text::{
+        prelude::{HorizontalAlign, VerticalAlign},
+        Text, Text2dBundle, TextAlignment, TextStyle,
+    },
+    transform::components::Transform,
+    DefaultPlugins,
+};
 
 fn main() {
     App::build()
@@ -10,5 +27,38 @@ fn main() {
         )))
         .add_plugins(DefaultPlugins)
         .add_plugins(AsteroidsGamePlugins)
+        .add_startup_system(setup.system())
+        .add_system(update_entity_count.exclusive_system())
         .run();
+}
+
+struct EntityCounter;
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands
+        // 2d camera
+        .spawn_bundle(Text2dBundle {
+            text: Text::with_section(
+                "N/A",
+                TextStyle {
+                    font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                    font_size: 60.0,
+                    color: Color::GREEN,
+                },
+                TextAlignment {
+                    vertical: VerticalAlign::Center,
+                    horizontal: HorizontalAlign::Center,
+                },
+            ),
+            transform: Transform::from_translation(Vec3::new(0.0, 280.0, 100.0)),
+            ..Default::default()
+        })
+        .insert(EntityCounter);
+}
+
+fn update_entity_count(mut world: &mut World) {
+    let mut query = world.query_filtered::<&mut Text, With<EntityCounter>>();
+    let value = (*world.entities()).len();
+    for mut counter in query.iter_mut(&mut world) {
+        counter.sections[0].value = value.to_string();
+    }
 }
