a7e15f6b858d8d3561b5cbd3f9ac500d25e8cb8e
Kevin d'Orange <kevin.dorange@protonmail.com>
2021-05-18 17:09:26 +0200

    squashme

---
+++ src/title.rs
@@ -0,0 +1,37 @@
+use crate::AppState;
+use bevy::{
+    app::{AppBuilder, Plugin},
+    core::Time,
+    ecs::{
+        schedule::State,
+        schedule::SystemSet,
+        system::{IntoSystem, Res, ResMut},
+    },
+    input::{keyboard::KeyCode, Input},
+};
+
+pub struct TitlePlugin;
+
+fn launch_game(time: Res<Time>, keyboard: Res<Input<KeyCode>>, mut state: ResMut<State<AppState>>) {
+    if keyboard.just_released(KeyCode::Return) {
+        state.push(AppState::Game).unwrap();
+    }
+}
+
+fn setup() {
+    println!("setup");
+}
+
+fn teardown() {
+    println!("teardown");
+}
+
+impl Plugin for TitlePlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_state(AppState::Title)
+            .add_system_set(SystemSet::on_update(AppState::Title).with_system(launch_game.system()))
+            .add_system_set(SystemSet::on_enter(AppState::Title).with_system(setup.system()))
+            .add_system_set(SystemSet::on_pause(AppState::Title).with_system(teardown.system()))
+            .add_system_set(SystemSet::on_exit(AppState::Title).with_system(teardown.system()));
+    }
+}
